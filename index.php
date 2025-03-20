afdbbb72838e54efa12f7c8f2979c8da27c907a9
OliverDugmore <oliver.dugmore@onecom.co.uk>
2025-03-20 09:31:34 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,70 +1,75 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Calculator</title>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		 
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-	</head>
-	<body>
-		
-		<div class="container" style="margin-top: 50px">
-		
-			<?php
-			
-				// If the submit button has been pressed
-				if(isset($_POST['submit']))
-				{
-					// Check number values
-					if(is_numeric($_POST['number1']) && is_numeric($_POST['number2']))
-					{
-						// Calculate total
-						if($_POST['operation'] == 'plus')
-						{
-							$total = $_POST['number1'] + $_POST['number2'];	
-						}
-						if($_POST['operation'] == 'minus')
-						{
-							$total = $_POST['number1'] - $_POST['number2'];	
-						}
-						if($_POST['operation'] == 'times')
-						{
-							$total = $_POST['number1'] * $_POST['number2'];	
-						}
-						if($_POST['operation'] == 'divided by')
-						{
-							$total = $_POST['number1'] / $_POST['number2'];	
-						}
-						
-						// Print total to the browser
-						echo "<h1>{$_POST['number1']} {$_POST['operation']} {$_POST['number2']} equals {$total}</h1>";
-					
-					} else {
-						
-						// Print error message to the browser
-						echo 'Numeric values are required';
-					
-					}
-				}
-			
-			?>
-		    
-		    <!-- Calculator form -->
-		    <form method="post" action="calculator.php">
-		        <input name="number1" type="text" class="form-control" style="width: 150px; display: inline" />
-		        <select name="operation">
-		        	<option value="plus">Plus</option>
-		            <option value="minus">Minus</option>
-		            <option value="times">Times</option>
-		            <option value="divided by">Divided By</option>
-		        </select>
-		        <input name="number2" type="text" class="form-control" style="width: 150px; display: inline" />
-		        <input name="submit" type="submit" value="Calculate" class="btn btn-primary" />
-		    </form>
-	    
-		</div>
-	
-	</body>
-</html>
+<!DOCTYPE html> 
+<html> 
+  
+<head> 
+    <title>HTML Calculator</title> 
+  
+    <!-- For styling -->
+    <style> 
+        table { 
+            border: 1px solid black; 
+            margin-left: auto; 
+            margin-right: auto; 
+        } 
+          
+        input[type="button"] { 
+            width: 100%; 
+            padding: 20px 40px; 
+            background-color: green; 
+            color: white; 
+            font-size: 24px; 
+            font-weight: bold; 
+            border: none; 
+            border-radius: 5px; 
+        } 
+          
+        input[type="text"] { 
+            padding: 20px 30px; 
+            font-size: 24px; 
+            font-weight: bold; 
+            border: none; 
+            border-radius: 5px; 
+            border: 2px solid black; 
+        } 
+    </style> 
+</head> 
+  
+<body> 
+  
+    <!-- Create table -->
+    <table id="calcu"> 
+        <tr> 
+            <td colspan="3"> 
+                <input type="text" id="result"> 
+            </td> 
+            <td><input type="button" value="c"></td> 
+        </tr> 
+  
+        <tr> 
+            <td><input type="button" value="1"></td> 
+            <td><input type="button" value="2"></td> 
+            <td><input type="button" value="3"></td> 
+            <td><input type="button" value="/"></td> 
+        </tr> 
+        <tr> 
+            <td><input type="button" value="4"></td> 
+            <td><input type="button" value="5"></td> 
+            <td><input type="button" value="6"></td> 
+            <td><input type="button" value="*"></td> 
+        </tr> 
+        <tr> 
+            <td><input type="button" value="7"></td> 
+            <td><input type="button" value="8"></td> 
+            <td><input type="button" value="9"></td> 
+            <td><input type="button" value="-"></td> 
+        </tr> 
+        <tr> 
+            <td><input type="button" value="0"></td> 
+            <td><input type="button" value="."></td> 
+            <td><input type="button" value="="></td> 
+            <td><input type="button" value="+"></td> 
+        </tr> 
+    </table> 
+</body> 
+  
+</html>  
